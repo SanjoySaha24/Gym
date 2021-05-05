3d4b0ca41fc164968782136e3680c1c412c28c08
Sanjoy Saha <sanjoysaha0524@gmail.com>
2021-05-06 00:28:24 +0530

    Hello

---
+++ index.js
@@ -0,0 +1,65 @@
+// var number1 = 34;
+// var number2 = 56;
+// console.log(number1 + number2);
+
+// var marks = {
+//     ravi : 34,
+//     sum : 09,
+//     po : 34.34
+// }
+// console.log(marks);
+
+
+// var a = true;
+// var b = false;
+// console.log(a, b);
+
+// var und = undefined;
+// console.log(und);
+
+// var arr = [1,2,3,4,5]
+
+// function avg(a,b){
+//     return (a+b)/2;
+// }
+// c = avg(4, 6);
+// console.log(c);
+
+
+// var age = 5;
+// if(age > 18){
+//     console.log("You can drink rasna water");
+// }
+//     else if(age > 88){
+//         console.log("You can drink rasna water");
+//     }
+//     else if(age > 48){
+//         console.log("You can drink rasna water");
+//     }
+//         else if(age > 98){
+//             console.log("You can drink rasna water");
+//         }
+//         else{
+//             console.log("Leave it");
+//         }
+
+// var arr = [1,2,3,4,5,6,7];
+
+//  let j=0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j++;
+// }
+
+// do{
+//     console.log(arr[j]);
+//          j++;
+// }while(j<arr.length)
+
+// let myDate = new Date();
+// console.log(myDate.getTime());
+// console.log(myDate.getDay());
+// console.log(myDate.getHours());
+
+let elemClass = document.getElementById('click');
+console.log(elem);
